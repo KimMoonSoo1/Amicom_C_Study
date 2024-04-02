d8cea586c773243307f297e2b2b17a182f2e4e0d
김문수 <ms1677@ajou.ac.kr>
2024-04-02 16:01:38 +0900

    [docs] Study_Python_3주차

---
+++ Assignment2.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS  
+#include <stdio.h>    
+int main() {
+
+	int j, i, N;
+	scanf("%d", &N);
+	for (i = 1; i <= N; i += 1) {
+		for (j = 1; j <= N - i; j++) {
+			printf(" ");
+		}
+		for (j = 1; j <= i; j += 1) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+
+	return 0;
+
+}
